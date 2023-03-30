d389efa9d2ca37eb09da22c935f624aa0cf9a25a
Eily <Eily@qq.com>
2023-03-30 13:21:34 +0800

    js commit

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 
-document.write("网页打印");
-
 // 延迟执行脚本
 var x = document.getElementById("yanchi");
 for(var i = 0 ; i < x.length ; i++){
     x[i].innerText = "延迟展示";
 }
+
+// 学习记录3.30
+window.alert("弹窗警告框");
+
+document.write("输出内容写到html");
+
+document.write(Date())
+
+
 
